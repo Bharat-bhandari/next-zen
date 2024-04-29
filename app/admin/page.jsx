6b817a633c35681e9c54c8fb3c2c792c7b44e73e
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-29 23:52:44 +0530

    add in page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { getIsAdmin } from "@/utils/getUserSession";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const isAdmin = await getIsAdmin();
+
+  console.log(isAdmin);
+
+  if (!isAdmin) return redirect("/");
+
   return <div>page</div>;
 };
 
